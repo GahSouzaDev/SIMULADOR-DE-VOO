5d42e9ba5acf620d27f9f564bcec391470a8ee81
GahSouzaDEV <emerdemolidor@gmail.com>
2025-07-04 20:21:28 -0300

    atualização

--- README.md
+++
@@ -1,1 +0,0 @@
-Link para GIFs: https://giphy.com/
--- controls.js
+++ controls.js
@@ -186,8 +186,8 @@ function animate() {
             const pitch = minPitch + (maxPitch - minPitch) * speedRatio;
             planeSound.setPlaybackRate(pitch);
 
-            const minVolume = 0.3;
-            const maxVolume = 0.7;
+            const minVolume = 0.2;
+            const maxVolume = 0.18;
             const volume = minVolume + (maxVolume - minVolume) * speedRatio;
             planeSound.setVolume(volume);
         } else if (planeSound.isPlaying) {
